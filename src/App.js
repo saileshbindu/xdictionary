60010790c38952e5dffe9857ace32d4d6496c6ba
saileshbindu <sailesh.bindu189@gmail.com>
2024-07-19 18:01:01 +0530

    xdictionary first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputVal, setInputVal] = useState('');
+  const [result, setResult] = useState(null)
+  const dictionary = 
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ]
+  
+  
+  const searchHandle = () =>{
+    const foundData = dictionary.find(ele => ele.word.toLowerCase() === inputVal.toLowerCase() );
+    setResult(foundData)
+    setInputVal('')
+    return foundData
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dictionary App</h1>
+      <div>
+      
+       <input 
+        type='text'
+        value={inputVal}
+      onChange={(e) => setInputVal(e.target.value)}
+        />
+        <button 
+          type='submit'
+          onClick={searchHandle}
+          >Search</button>
+       
+      </div>
+      <h3>Definition</h3>
+      {result ? <p>{result.meaning}</p> : <p>Word not found in the dictionary.</p>}
     </div>
   );
 }
