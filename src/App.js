7c81aff7ff4d7a79727cc45954e8b6ca9e4bebaf
saileshbindu <sailesh.bindu189@gmail.com>
2024-08-07 15:15:06 +0530

    new code upated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [inputVal, setInputVal] = useState('');
   const [result, setResult] = useState('');
+  const [showResult, setShowResult] = useState(false);
 
   const dictionary = [
     { word: "React", meaning: "A JavaScript library for building user interfaces." },
@@ -14,28 +15,28 @@ function App() {
   const searchHandle = () => {
     if (inputVal.trim() === '') {
       setResult('Word not found in the dictionary.');
+      setShowResult(true);
       return;
     }
-
     const foundData = dictionary.find(ele => ele.word.toLowerCase() === inputVal.toLowerCase());
-    setResult(foundData ? foundData.meaning : 'Word not found in the dictionary.')
-  
+    setResult(foundData ? foundData.meaning : 'Word not found in the dictionary.');
+    setShowResult(true);
+    setInputVal('');
   };
 
   return (
     <div className="App">
       <h1>Dictionary App</h1>
       <div>
-        <input
-          type='text'
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-          placeholder='Search for a word ...'
-        />
+        <input type='text' value={inputVal} onChange={(e) => setInputVal(e.target.value)} placeholder='Search for a word ...' />
         <button type='submit' onClick={searchHandle}>Search</button>
       </div>
-      <p><strong>Definition: {result}</strong></p>
-   
+      {showResult && (
+        <p>
+          <strong>Definition: </strong>
+          {result}
+        </p>
+      )}
     </div>
   );
 }
